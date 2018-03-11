cb855a52824a8cd549c84c4827d9b5100fe8cbfb
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-11 20:07:57 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 def key_for_min_value(name_hash)
 
   sorted_h = name_hash.sort_by { |key, val| val}.to_h
-  if sorted_h.nil?
+  if sorted_h == {}
     return nil
   else
      sorted_h.each do |k, v|
