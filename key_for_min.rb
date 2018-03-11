443343ec192301f19cb40d5e6b56f190a9e7cc38
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-11 19:56:07 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 def key_for_min_value(name_hash)
 
   sorted_h = name_hash.sort_by { |key, val| val}.to_h
-  sorted_h.each do |k, v| 
-    return v
+  sorted_h.each do |k, v|
+    return k
   end
 
 
